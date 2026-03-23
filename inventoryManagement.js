249eeaa796eb18389153fa51034cf639e444782b
daudikombe <daudikombe77@gmail.com>
2026-03-23 21:20:55 +0300

    initial commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,38 @@
 // Write your code here
 
+// The products array
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+// Function to log the first product
+function logFirstProduct() {
+    console.log(products[0]);
+}
+
+// Function to update product name at a specific index
+function updateProductName(index, newName) {
+    if (index >= 0 && index < products.length) {
+        products[index] = newName;
+    }
+}
+
+// Function to remove the last product
+function removeLastProduct() {
+    products.pop();
+}
+
+// Function to add a new product
+function addProduct(newProduct) {
+    products.push(newProduct);
+}
+
+// Export the functions and array for testing
+module.exports = {
+    products,
+    logFirstProduct,
+    updateProductName,
+    removeLastProduct,
+    addProduct
+};
 
 
 // Export the necessary parts for testing
